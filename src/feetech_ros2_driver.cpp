771f5b34d8e0426bf850e62b6be8b7052819ffd0
Tsogoo <ts.r.madrid@gmail.com>
2025-06-18 19:18:03 +0800

    Add on_deactivate function to disable torque when exit (#12)

--- src/feetech_ros2_driver.cpp
+++ src/feetech_ros2_driver.cpp
@@ -174,6 +174,19 @@ CallbackReturn FeetechHardwareInterface::on_activate(const rclcpp_lifecycle::Sta
   return CallbackReturn::SUCCESS;
 }
 
+CallbackReturn FeetechHardwareInterface::on_deactivate(const rclcpp_lifecycle::State& /* previous_state */) {
+  // all joints torque off
+  const auto torque_disable_parameters =
+      std::vector(joint_ids_.size(), std::experimental::make_array(static_cast<uint8_t>(0)));
+  if (const auto result =
+          communication_protocol_->sync_write(joint_ids_, SMS_STS_TORQUE_ENABLE, torque_disable_parameters);
+      !result) {
+    spdlog::error("FeetechHardwareInterface::on_deactivate -> {}", result.error());
+    return CallbackReturn::ERROR;
+  }
+  return CallbackReturn::SUCCESS;
+}
+
 }  // namespace feetech_ros2_driver
 
 #include "pluginlib/class_list_macros.hpp"
